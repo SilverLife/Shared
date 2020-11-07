560354cae647ccda53e9b3b5c25174d0c1985bfa
Andre <Silver_life@mail.ru>
2020-11-07 12:29:30 +0300

    Move engine to shared

---
+++ EventGameEngine/FieldData/FieldObjectWithPos.h
@@ -0,0 +1,33 @@
+﻿// ☕ Привет
+#pragma once
+
+#include "FieldObject.h"
+
+namespace EventGameEngine
+{
+	namespace FieldData
+	{
+		class FieldObjectWithPos : public FieldObject
+		{
+		protected:
+			Point _pos;
+		public:
+			FieldObjectWithPos(Point pos)
+				: FieldObject()
+				, _pos(pos)
+			{}
+
+			FieldObjectWithPos(Point pos, char draw_symbol, Shared::ConsoleHelper::Color color)
+				: FieldObject(draw_symbol, color) 
+				, _pos(pos)
+			{}
+
+			void OnMove(Point prev_pos, Point new_pos) override
+			{
+				_pos = new_pos;
+			}
+
+			Point Pos() const { return _pos; }
+		};
+	}
+}
