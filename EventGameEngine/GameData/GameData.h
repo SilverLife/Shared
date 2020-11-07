f94f93a022976914fe9238887f1a4ccacfaacb8f
Andre <Silver_life@mail.ru>
2020-11-07 14:11:44 +0300

    Add game data

---
+++ EventGameEngine/GameData/GameData.h
@@ -0,0 +1,23 @@
+﻿// ☕ Привет
+#pragma once
+
+#include "../FieldData/Field.h"
+#include "../ActionData/ActionManager.h"
+#include "../EventData/EventManager.h"
+
+namespace EventGameEngine
+{
+	template <size_t kCustomEventsCount>
+	class GameData
+	{
+	public:
+		EventGameEngine::FieldData::Field _field;
+		Action::ActionManager<kCustomEventsCount> _action_manager;
+		EventData::EventManager _event_manager;
+
+		GameData(Size field_size)
+			: _field(field_size)
+			, _action_manager(&_event_manager, &_field)
+		{}
+	};
+}
